d97f0693b84be0af50ee022aa98189ae998f2966
Royce Townsend <git@roycetownsend.com>
2021-01-11 14:52:14 +0800

    Switch pages to typescript and use theme-ui styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+// markup
+const IndexPage = () => {
+  return (
+    <>
+      <h1>Heading</h1>
+      <p>
+        Paragraph in <code>/index.tsx</code>
+      </p>
+      <h2>Heading</h2>
+    </>
+  );
+};
+
+export default IndexPage;
